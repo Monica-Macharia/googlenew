8a7a506cb280aa384029812918380347431f0865
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2023-05-09 07:19:20 +0300

    function

--- index.js
+++ index.js
@@ -24,3 +24,18 @@ const contacts = [
       likes: ["JavaScript", "Gaming", "Foxes"],
     },
   ];
+
+  console.log(contacts[firstName])
+
+  function lookUpProfile(name, prop) {
+    for (let x = 0; x < contacts.length; x++) {
+      if (contacts[x].firstName === name) {
+        if (contacts[x].hasOwnProperty(prop)) {
+          return contacts[x][prop];
+        } else {
+          return "No such property";
+        }
+      }
+    }
+    return "No such contact";
+  }
